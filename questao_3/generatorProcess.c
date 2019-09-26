767841f1a4efc024201ac3397a1bec7b11b8b685
marcosbmf <marcos.barros.filho@ccc.ufcg.edu.br>
2019-09-26 12:05:14 -0300

    Adding error handling for forks

--- questao_3/generatorProcess.c
+++ questao_3/generatorProcess.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -8,8 +9,11 @@ int main(int argc, char *argv[]) {
 	int signal;
 	int cpid;
 	for (int i = 0; i < n; i++) {
-		if (fork() == 0) {
+		int forkStatus = fork();
+		if (forkStatus == 0) {
 			exit(0);
+		} else if (forkStatus == -1) {
+			printf("Error creating process");
 		}
 	}
 	do {
