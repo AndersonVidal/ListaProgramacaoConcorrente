e1c39e24bf8ce9d4e0e8ce60df484e32c904eda0
marcosbmf <marcos.barros.filho@ccc.ufcg.edu.br>
2019-09-26 11:57:08 -0300

    Fixing process generation and waiting

--- questao_3/generatorProcess.c
+++ questao_3/generatorProcess.c
@@ -6,11 +6,13 @@
 int main(int argc, char *argv[]) {
 	int n = atoi(argv[1]);
 	int signal;
+	int cpid;
 	for (int i = 0; i < n; i++) {
 		if (fork() == 0) {
 			exit(0);
-		} else {
-			wait(&signal);	
 		}
 	}
+	do {
+		cpid = wait(&signal);
+	} while (cpid > 0);
 }
