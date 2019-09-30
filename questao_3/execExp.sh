bab5e91de63f8ada181d16190742c461c81888a8
marcosbmf <marcos.barros.filho@ccc.ufcg.edu.br>
2019-09-30 11:08:57 -0300

    New script to run tests and output to file

--- questao_3/execExp.sh
+++ questao_3/execExp.sh
@@ -11,30 +11,30 @@ echo "======= Time consume ======="
 echo " *** Threads *** "
 
 echo "# N = 100"
-time ./gt 100
+perf stat --repeat=1000 -o threads_100.txt ./gt 100
 echo""
 
 echo "# N = 1000"
-time ./gt 1000
+perf stat --repeat=1000 -o threads_1k.txt ./gt 1000
 echo""
 
 echo "# N = 10000"
-time ./gt 10000
+perf stat --repeat=1000 -o threads_10k.txt ./gt 10000
 echo""
 
 echo""
 
 echo " *** Process *** "
 echo "# N = 100"
-time ./gp 100
+perf stat --repeat=1000 -o proc_100.txt ./gp 100
 echo""
 
 echo "# N = 1000"
-time ./gp 1000
+perf stat --repeat=1000 -o proc_1k.txt ./gp 1000
 echo""
 
 echo "# N = 10000"
-time ./gp 10000
+perf stat --repeat=5 -o proc_10k.txt ./gp 10000 ## Couldn't repeat more.
 echo""
 
 
