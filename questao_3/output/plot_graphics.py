dcf584d37495f6ab47bbcfcd475328621b4b464e
Mateus Queiroz Cunha <30473087+mateusqc@users.noreply.github.com>
2019-10-10 17:46:36 -0300

    boxplots separados p/ cada unid. de milhar + hist

--- questao_3/output/plot_graphics.py
+++ questao_3/output/plot_graphics.py
@@ -96,7 +96,7 @@ plt.plot(keys, mean_proc_time_list, label='Processos')
 plt.plot(keys, mean_thread_time_list, label='Threads')
 plt.xlabel('Número de instâncias')
 plt.ylabel('Tempo (s)')
-plt.title('Tempo médio de criação')
+plt.title('Tempo total médio de criação')
 plt.legend()
 plt.savefig('rect_proc_x_thread_time.png', bbox_inches='tight')
 plt.clf()
@@ -105,11 +105,36 @@ plt.plot(keys, mean_proc_mem_list, label='Processos')
 plt.plot(keys, mean_thread_mem_list, label='Threads')
 plt.xlabel('Número de instâncias')
 plt.ylabel('Memória Utilizada (KB)')
-plt.title('Quantidade média de memória utilizada')
+plt.title('Quantidade total média de memória utilizada')
 plt.legend()
 plt.savefig('rect_proc_x_thread_mem.png', bbox_inches='tight')
 plt.clf()
 
+## PER-THREAD AND PER-PROCESS DATA HISTOGRAMS
+
+thread_hist_mem = [statistics.mean(np.array(thread_mem_data_map[key]) / key) for key in keys]
+thread_hist_time = [statistics.mean(np.array(thread_time_data_map[key]) / key) for key in keys]
+proc_hist_mem = [statistics.mean(np.array(proc_mem_data_map[key]) / key) for key in keys]
+proc_hist_time = [statistics.mean(np.array(proc_time_data_map[key]) / key) for key in keys]
+
+print(thread_hist_mem)
+print(thread_hist_time)
+print(proc_hist_mem)
+print(proc_hist_time)
+
+fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
+# We can set the number of bins with the `bins` kwarg
+axs[0].hist(thread_hist_mem, bins=20)
+axs[1].hist(proc_hist_mem, bins=20)
+plt.savefig('hist_mem.png', bbox_inches='tight')
+plt.clf()
+
+fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
+# We can set the number of bins with the `bins` kwarg
+axs[0].hist(thread_hist_time, bins=20)
+axs[1].hist(proc_hist_time, bins=20)
+plt.savefig('hist_time.png', bbox_inches='tight')
+plt.clf()
 
 ## BOXPLOT GRAPHS ##
 
@@ -130,8 +155,8 @@ ax[1].set_xticklabels(keys[:5])
 ax[0].set_title('Boxplots da quantidade de memória utilizada por Threads')
 ax[1].set_title('Boxplots da quantidade de memória utilizada por Processos')
 # Setting the scale to facilitate the data comparisson
-ax[0].set_ylim(top=ax[1].get_ylim()[1])
-ax[1].set_ylim(bottom=ax[0].get_ylim()[0])
+# ax[0].set_ylim(top=ax[1].get_ylim()[1])
+# ax[1].set_ylim(bottom=ax[0].get_ylim()[0])
 for axe in ax:
     axe.yaxis.grid(True)
     axe.set_xlabel('Número de instâncias')
@@ -149,8 +174,8 @@ ax[1].set_xticklabels(keys[:5])
 ax[0].set_title('Boxplots do tempo de criação de Threads')
 ax[1].set_title('Boxplots do tempo de criação de Processos')
 # Setting the scale to facilitate the data comparisson
-ax[0].set_ylim(top=ax[1].get_ylim()[1])
-ax[1].set_ylim(bottom=ax[0].get_ylim()[0])
+# ax[0].set_ylim(top=ax[1].get_ylim()[1])
+# ax[1].set_ylim(bottom=ax[0].get_ylim()[0])
 for axe in ax:
     axe.yaxis.grid(True)
     axe.set_xlabel('Número de instâncias')
@@ -171,8 +196,8 @@ ax[0].set_title('Boxplots da quantidade de memória utilizada por Threads')
 ax[1].set_title('Boxplots da quantidade de memória utilizada por Processos')
 
 # Setting the scale to facilitate the data comparisson
-ax[0].set_ylim(top=ax[1].get_ylim()[1])
-ax[1].set_ylim(bottom=ax[0].get_ylim()[0])
+# ax[0].set_ylim(top=ax[1].get_ylim()[1])
+# ax[1].set_ylim(bottom=ax[0].get_ylim()[0])
 for axe in ax:
     axe.yaxis.grid(True)
     axe.set_xlabel('Número de instâncias')
@@ -190,8 +215,8 @@ ax[1].set_xticklabels(keys[5:10])
 ax[0].set_title('Boxplots do tempo de criação de Threads')
 ax[1].set_title('Boxplots do tempo de criação de Processos')
 # Setting the scale to facilitate the data comparisson
-ax[0].set_ylim(top=ax[1].get_ylim()[1])
-ax[1].set_ylim(bottom=ax[0].get_ylim()[0])
+# ax[0].set_ylim(top=ax[1].get_ylim()[1])
+# ax[1].set_ylim(bottom=ax[0].get_ylim()[0])
 for axe in ax:
     axe.yaxis.grid(True)
     axe.set_xlabel('Número de instâncias')
@@ -211,8 +236,8 @@ ax[1].set_xticklabels(keys[10:15])
 ax[0].set_title('Boxplots da quantidade de memória utilizada por Threads')
 ax[1].set_title('Boxplots da quantidade de memória utilizada por Processos')
 # Setting the scale to facilitate the data comparisson
-ax[0].set_ylim(top=ax[1].get_ylim()[1])
-ax[1].set_ylim(bottom=ax[0].get_ylim()[0])
+# ax[0].set_ylim(top=ax[1].get_ylim()[1])
+# ax[1].set_ylim(bottom=ax[0].get_ylim()[0])
 for axe in ax:
     axe.yaxis.grid(True)
     axe.set_xlabel('Número de instâncias')
@@ -228,8 +253,8 @@ bp1 = ax[1].boxplot(proc_box_time[10:15])
 ax[0].set_xticklabels(keys[10:15])
 ax[1].set_xticklabels(keys[10:15])
 # Setting the scale to facilitate the data comparisson
-ax[0].set_ylim(top=ax[1].get_ylim()[1])
-ax[1].set_ylim(bottom=ax[0].get_ylim()[0])
+# ax[0].set_ylim(top=ax[1].get_ylim()[1])
+# ax[1].set_ylim(bottom=ax[0].get_ylim()[0])
 ax[0].set_title('Boxplots do tempo de criação de Threads')
 ax[1].set_title('Boxplots do tempo de criação de Processos')
 for axe in ax:
@@ -251,8 +276,8 @@ ax[1].set_xticklabels(keys[15:])
 ax[0].set_title('Boxplots da quantidade de memória utilizada por Threads')
 ax[1].set_title('Boxplots da quantidade de memória utilizada por Processos')
 # Setting the scale to facilitate the data comparisson
-ax[0].set_ylim(top=ax[1].get_ylim()[1])
-ax[1].set_ylim(bottom=ax[0].get_ylim()[0])
+# ax[0].set_ylim(top=ax[1].get_ylim()[1])
+# ax[1].set_ylim(bottom=ax[0].get_ylim()[0])
 for axe in ax:
     axe.yaxis.grid(True)
     axe.set_xlabel('Número de instâncias')
@@ -270,14 +295,35 @@ ax[1].set_xticklabels(keys[15:])
 ax[0].set_title('Boxplots do tempo de criação de Threads')
 ax[1].set_title('Boxplots do tempo de criação de Processos')
 # Setting the scale to facilitate the data comparisson
-ax[0].set_ylim(top=ax[1].get_ylim()[1])
-ax[1].set_ylim(bottom=ax[0].get_ylim()[0])
+# ax[0].set_ylim(top=ax[1].get_ylim()[1])
+# ax[1].set_ylim(bottom=ax[0].get_ylim()[0])
 for axe in ax:
     axe.yaxis.grid(True)
     axe.set_xlabel('Número de instâncias')
     axe.set_ylabel('Tempo (s)')
 plt.savefig('boxplot_6K_10K_time.png', bbox_inches='tight')
 plt.clf()
+
+
+## SEPARATED MEMORY BOXPLOTS - 1K to 10K
+
+for i in range(10):
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(18, 8))
+    bp0 = ax[0].boxplot(thread_box_mem[i + 10])
+    bp1 = ax[1].boxplot(proc_box_mem[i + 10])
+    ax[0].set_xticklabels(keys[i + 10: i + 11])
+    ax[1].set_xticklabels(keys[i + 10: i + 11])
+    ax[0].set_title('Boxplot da quantidade de memória utilizada por Threads')
+    ax[1].set_title('Boxplot da quantidade de memória utilizada por Processos')
+    # Setting the scale to facilitate the data comparisson
+    # ax[0].set_ylim(top=ax[1].get_ylim()[1])
+    # ax[1].set_ylim(bottom=ax[0].get_ylim()[0])
+    for axe in ax:
+        axe.yaxis.grid(True)
+        axe.set_xlabel('Número de instâncias')
+        axe.set_ylabel('Memória Utilizada (KB)')
+    plt.savefig('boxplot_' + str(i + 1) + 'K_mem.png', bbox_inches='tight')
+    plt.clf()
 
 ## INCREASE PERCENTAGE GRAPH ##
 
@@ -295,7 +341,7 @@ rects1 = ax.bar(np.array(range(0, len(keys))) - column_width / 2, np.array(mem_i
 rects2 = ax.bar(np.array(range(0, len(keys))) + column_width / 2, np.array(time_increase_perc), column_width, label='Tempo (s)')
 ax.set_ylabel('Taxa de variação (%)')
 ax.set_xlabel('Número de instâncias')
-ax.set_title('Aumento dos valores de tempo e memória com múltiplas instâncias de threads para processos')
+ax.set_title('Taxa de variação de tempo e memória de processos em relação à threads')
 ax.set_xticks(np.array(range(0, len(keys))))
 ax.set_xticklabels(keys)
 ax.legend(loc='upper left', fontsize='x-large')
